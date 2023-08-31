56c4c4becd2cbe8342362535b93456d403f4d1b8
Nicholas Sorak <sorakn@ad.lafayette.edu>
2023-08-31 14:27:31 -0400

    git ignore and other changes

--- lab01/RTL/sevenseg_ext_top.sv
+++ lab01/RTL/sevenseg_ext_top.sv
@@ -1,10 +1,10 @@
-module  sevenseg_ext_n(input logic [6:0] data,
+module  sevenseg_ext_top(input logic [6:0] data,
  output logic [6:0] segs_n,
  output logic dp_n,
   output logic selected);
 
 assign selected = 1;
-sevenseg_ext_n(data, segs_n, dp_n);
+sevenseg_ext_n sevseg(data, segs_n, dp_n);
 
 
 
