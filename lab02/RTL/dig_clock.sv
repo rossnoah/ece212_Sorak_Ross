98240621086c066053ec7e7a2d9d9d49c585d071
Noah Ross <me@noah.dev>
2023-09-14 15:45:42 -0400

    working lab2

--- lab02/RTL/dig_clock.sv
+++ lab02/RTL/dig_clock.sv
@@ -25,31 +25,45 @@ module dig_clock(
     input logic rst,
     input logic adv_hr,
     input logic adv_min,
-    output logic [6:0] segs_n,
-    output logic dp_n,
-    output logic [7:0] an_n
+    output logic [6:0] secondOne, secondTen, minuteOne, minuteTen, hourOne, hourTen,ampmOne
     );
     
-    logic enb, clr;
-    period_enb #(.PERIOD_MS(1)) U_ENB(.clk, .rst, .clr, .enb_out(enb));
+  logic [5:0] seconds, minutes, hours, hoursMapped, ampm;
+  logic enb, clr, minuteEnb, hourEnb, amEnb;
 
-    
-//    counter_rc_mod count_sec(.clk,.rst,.enb,);
-    //counter_rc_mod count_min();
-  //  counter_rc_mod count_hr();
-    
-    
-    
-    
-//    module dbl_dabble(
-//    input logic [7:0] b,
-//    output logic [3:0] hundreds, tens, ones
-//    );
+  assign hoursMapped = (hours == 0) ? 12 : hours;
 
-    
-    
-     //   input logic clk, rst, enb,
-   // output logic [5:0] q,
- //   output logic cy
+
+  logic adv_hr_db, adv_min_db, adv_hr_pulsed, adv_min_pulsed;
+
+  debounce hr (.clk, .rst, .pb(adv_hr), .pb_debounced(adv_hr_db));
+  debounce min (.clk, .rst, .pb(adv_min), .pb_debounced(adv_min_db));
+
+  single_pulser hr_pulse (.clk, .din(adv_hr_db), .d_pulse(adv_hr_pulsed));
+  single_pulser min_pulse (.clk, .din(adv_min_db), .d_pulse(adv_min_pulsed));
+
+
+
+  period_enb #(.PERIOD_MS(1000)) U_ENB(.clk, .rst, .clr, .enb_out(enb));
+
+  counter_rc_mod #(.MOD(60)) COUNT_SEC(.clk,.rst,.enb,.q(seconds),.cy(minuteEnb));
+  counter_rc_mod #(.MOD(60)) COUNT_MIN(.clk,.rst,.enb(minuteEnb||adv_min_pulsed),.q(minutes),.cy(hourEnb));
+  counter_rc_mod #(.MOD(12)) COUNT_HR(.clk,.rst,.enb(hourEnb||adv_hr_pulsed),.q(hours),.cy(amEnb));
+  counter_rc_mod #(.MOD(2)) COUNT_AM(.clk,.rst,.enb(amEnb),.q(ampm));
+
+
+  dbl_dabble secondOut(.b(seconds),.ones(secondOne[3:0]),.tens(secondTen[3:0]));
+  dbl_dabble minuteOut(.b(minutes),.ones(minuteOne[3:0]),.tens(minuteTen[3:0]));
+  dbl_dabble hourOut(.b(hoursMapped),.ones(hourOne[3:0]),.tens(hourTen[3:0]));
+
+  assign ampmOne[3:0] = ampm+10;
+  assign ampmOne[6:4] = 3'b000;
+  assign secondOne[6:4] = 3'b000;
+  assign secondTen[6:4] = 3'b000;
+  assign minuteOne[6:4] = 3'b010;
+  assign minuteTen[6:4] = 3'b000;
+  assign hourOne[6:4] = 3'b010;
+  assign hourTen[5:4] = 2'b00;
+  assign hourTen[6] = hourTen[3:0] ? 1'b0 : 1'b1 ;
     
 endmodule
--- lab02/RTL/dig_clock_top.sv
+++ lab02/RTL/dig_clock_top.sv
@@ -8,12 +8,12 @@ module dig_clock_top (
     output logic [7:0] an_n
 );
 
-    logic sclk; // sclk stands for "slow clock"
-    clkdiv #(.DIVFREQ(1000)) U_DIV (.clk, .reset(0), .sclk);
-    
-    dig_clock(.clk(sclk), .rst, .adv_hr, .adv_min, .segs_n, .dp_n, .an_n);
+    logic [6:0] secondOne, secondTen, minuteOne, minuteTen, hourOne, hourTen,ampmOne  ;
 
-    sevenseg_ctl ctl(.clk(clk), .d7(7'd7), .d6(7'd6), .d5(7'd5), .d4(7'd4), .d3(7'd3), .d2(7'd2), .d1(7'b1000000), .d0(7'd0), .segs_n(segs_n), .dp_n(dp_n), .an_n(an_n));
+    
+    dig_clock(.clk(clk), .rst, .adv_hr, .adv_min, .secondOne, .secondTen, .minuteOne, .minuteTen, .hourOne, .hourTen, .ampmOne);
+    
+    sevenseg_ctl ctl(.clk(clk), .d7(hourTen), .d6(hourOne), .d5(minuteTen), .d4(minuteOne), .d3(secondTen), .d2(secondOne), .d1(7'b1000000), .d0(ampmOne), .segs_n(segs_n), .dp_n(dp_n), .an_n(an_n));
 
 
 endmodule
