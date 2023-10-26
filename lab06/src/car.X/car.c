016711f495edeed60fa630cce0e231ea895b6a1c
Noah Ross <me@noah.dev>
2023-10-26 13:21:24 -0400

    Revert "changes to lab6"
    This reverts commit 7658fbb65197e4f01714d1cef369eaf6f0250ac0.

--- lab06/src/car.X/car.c
+++ lab06/src/car.X/car.c
@@ -1,65 +1,4 @@
 #include "ece212.h"
-
-void filterSensors()
-{
-    if (rightSensor)
-    {
-        if (rDelay == 0)
-        {
-            rDelay = 500;
-            rS = 1;
-        }
-        else
-        {
-            rDelay--;
-        }
-    }
-    else
-    {
-        rDelay = 500;
-        rS = 0;
-    }
-
-    if (leftSensor)
-    {
-        if (lDelay == 0)
-        {
-            lDelay = 500;
-            lS = 1;
-        }
-        else
-        {
-            lDelay--;
-        }
-    }
-    else
-    {
-        lDelay = 500;
-        lS = 0;
-    }
-}
-
-void straight()
-{
-    // go straight
-    RFORWARD = 65535;
-    LFORWARD = 65535;
-}
-
-void right()
-{
-    // turn right
-    RFORWARD = 65535 / 2.3;
-    LFORWARD = 65535;
-}
-
-void left()
-{
-    // turn left
-    RFORWARD = 65535;
-    LFORWARD = 65535 / 2.3;
-}
-
 int main()
 {
     ece212_lafbot_setup();
@@ -73,30 +12,29 @@ int main()
     // LFORWARD = 65535;
     // delayms(50);
     // RFORWARD = 65535;
-    int rDelay = 20 * 625;
-    int lDelay = 20 * 625;
-    int rS = 0;
-    int lS = 0;
 
     while (1)
     {
         leftSensor = analogRead(LEFT_SENSOR) > 340 ? 1 : 0;
         rightSensor = analogRead(RIGHT_SENSOR) > 340 ? 1 : 0;
 
-        filterSensors();
-
-        if (lS && !rS)
+        if (leftSensor && !rightSensor)
         {
-            straight();
+            // go straight
+            RFORWARD = 65535;
+            LFORWARD = 65535;
         }
-
-        else if (!lS && !rS)
+        else if (!leftSensor && !rightSensor)
         {
-            left();
+            // turn left
+            RFORWARD = 65535;
+            LFORWARD = 0;
         }
-        else if ((!lS && rS) || (lS && rS))
+        else if ((!leftSensor && rightSensor) || (leftSensor && rightSensor))
         {
-            right();
+            // turn right
+            RFORWARD = 0;
+            LFORWARD = 65535;
         }
     }
     return (EXIT_SUCCESS);
